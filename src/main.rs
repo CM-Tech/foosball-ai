68c5bc51055fd036a2a1daab42a74f6af022a362
modderme123 <modderme123@gmail.com>
2018-01-20 10:28:18 -0800

    fmt

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ struct Point {
 
 #[derive(Debug)]
 struct Ball {
-    radius:f32,
-    position:Point,
-    velocity:Point
+    radius: f32,
+    position: Point,
+    velocity: Point,
 }
 
 #[derive(Debug)]
 struct World {
-    size:Point,
-    ball:Ball
+    size: Point,
+    ball: Ball,
 }
 /*const pallettes = [
     [0x85D600, 0xDE4A1F, 0xc4ff66, 0xe98263],
@@ -25,28 +25,34 @@ struct World {
 ];*/
 
 fn main() {
-    let mut world:World= World{size:Point{x:640.0,y:480.0},ball:Ball{radius:0.05,position:Point{x:0.5,y:0.5},velocity:Point{x:0.0,y:0.0}}};
+    let mut world: World = World {
+        size: Point { x: 640.0, y: 480.0 },
+        ball: Ball {
+            radius: 0.05,
+            position: Point { x: 0.5, y: 0.5 },
+            velocity: Point { x: 0.0, y: 0.0 },
+        },
+    };
     let mut window: PistonWindow = WindowSettings::new("Foosball", (640, 480))
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
             clear([0.9, 0.9, 0.9, 1.0], g);
-            rectangle([0.15, 0.72, 0.95, 1.0], // blue
+            rectangle([0.15, 0.72, 0.95, 1.0],  // blue
                       [0.0, 0.0, 100.0, 100.0], // rectangle
                       c.transform, g);
-            rectangle([0.95, 0.6, 0.13, 1.0], // orange
+            rectangle([0.95, 0.6, 0.13, 1.0],     // orange
                       [0.0, 100.0, 100.0, 100.0], // rectangle
                       c.transform, g);
 
-            rectangle([0.52, 0.84, 0.0, 1.0], // green
+            rectangle([0.52, 0.84, 0.0, 1.0],     // green
                       [100.0, 0.0, 100.0, 100.0], // rectangle
                       c.transform, g);
-            rectangle([0.87, 0.29, 0.12, 1.0], // red
+            rectangle([0.87, 0.29, 0.12, 1.0],      // red
                       [100.0, 100.0, 100.0, 100.0], // rectangle
                       c.transform, g);
-                      
         });
     }
 }
