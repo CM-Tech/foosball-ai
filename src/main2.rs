43bea974d045b8007e2eccce9b83018f7e8e2e64
Cole Kissane <coler706@gmail.com>
2018-01-20 16:49:48 -0800

    stuff

--- src/main2.rs
+++ src/main2.rs
@@ -1,91 +1,201 @@
-extern crate piston;
+extern crate find_folder;
 extern crate piston_window;
+
 use piston_window::*;
-use piston::input::*;
 use std::f64;
 
-
-
-#[derive(Debug)]
 struct Ball {
     radius: f64,
     position: (f64, f64),
     velocity: (f64, f64),
 }
 
-#[derive(Debug)]
 struct Player {
     score: u32,
+    dir: f32,
     position: f64,
 }
 
-#[derive(Debug)]
 struct World {
-    size: (u32, u32),
     ball: Ball,
     p1: Player,
     p2: Player,
 }
 
-/*const pallettes = [
-    [0x85D600, 0xDE4A1F, 0xc4ff66, 0xe98263],
-    [0x26B8F2, 0xF29A21, 0x90dbf9, 0xf7c47d]
-];*/
-
+const SCREEN: (u32, u32) = (1050, 600);
+const PALLETTES: [[[f32; 4]; 2]; 2] = [
+    [[0.15, 0.72, 0.95, 1.0], [0.95, 0.6, 0.13, 1.0]],
+    [[0.52, 0.84, 0.0, 1.0], [0.87, 0.29, 0.12, 1.0]],
+];
+const PLAYERS: [usize; 8] = [0, 0, 1, 0, 1, 0, 1, 1];
 fn main() {
-    let pallettes = [
-        [[0.15, 0.72, 0.95, 1.0], [0.95, 0.6, 0.13, 1.0]],
-        [[0.52, 0.84, 0.0, 1.0], [0.87, 0.29, 0.12, 1.0]],
-    ];
-    let my_palette = pallettes[0];
-    let colors: Vec<_> = [0, 0, 1, 0, 1, 0, 1, 1]
-        .iter()
-        .map(|x| my_palette[*x as usize])
-        .collect();
+    let my_palette = PALLETTES[0];
+
+    let w = SCREEN.0 as f64;
+    let h = SCREEN.1 as f64;
 
     let mut world: World = World {
-        size: (1050, 600),
         ball: Ball {
-            radius: 0.05,
-            position: (0.5, 0.5),
-            velocity: (0.0, 0.0),
+            radius: 10.0,
+            position: (w / 2.0, h / 2.0),
+            velocity: (1.0, 1.0),
         },
         p1: Player {
             score: 0,
-            position: 0.5,
+            dir: 0.0,
+            position: 0.0,
         },
         p2: Player {
             score: 0,
-            position: 0.5,
+            dir: 0.0,
+            position: 0.0,
         },
     };
-    let mut window: PistonWindow = WindowSettings::new("Foosball", world.size)
+    let mut window: PistonWindow = WindowSettings::new("Foosball", SCREEN)
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
-        let mut n:f64=0.0;
+        .unwrap();
+
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets")
+        .unwrap();
+    println!("{:?}", assets);
+    let ref font = assets.join("FiraSans-Regular.ttf");
+    let factory = window.factory.clone();
+    let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+
     while let Some(e) = window.next() {
-        n=n+0.01;
+        let speed = 0.05;
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::W => world.p1.dir = -speed,
+                Key::S => world.p1.dir = speed,
+                Key::Up => world.p2.dir = -speed,
+                Key::Down => world.p2.dir = speed,
+                _ => (),
+            }
+        }
+        if let Some(Button::Keyboard(key)) = e.release_args() {
+            match key {
+                Key::W if world.p1.dir == -speed => world.p1.dir = 0.0,
+                Key::S if world.p1.dir == speed => world.p1.dir = 0.0,
+                Key::Up if world.p2.dir == -speed => world.p2.dir = 0.0,
+                Key::Down if world.p2.dir == speed => world.p2.dir = 0.0,
+                _ => (),
+            }
+        }
         window.draw_2d(&e, |c, g| {
             clear([0.9, 0.9, 0.9, 1.0], g);
 
-            for (column, amount) in [1, 2, 3, 4, 4, 3, 2, 1].iter().enumerate() {
-                for y in 0..*amount {
-                    let w = world.size.0 as f64;
-                    let h = world.size.1 as f64;
+            world.p1.position += world.p1.dir as f64;
+            world.p2.position += world.p2.dir as f64;
+
+            world.p1.position = world.p1.position.min(1.0).max(-1.0);
+            world.p2.position = world.p2.position.min(1.0).max(-1.0);
+
+            world.ball.position.0 += world.ball.velocity.0 * 5.0;
+            world.ball.position.1 += world.ball.velocity.1 * 5.0;
+
+            if world.ball.position.1 - world.ball.radius > h / 3.0
+                && world.ball.position.1 + world.ball.radius < h * 2.0 / 3.0
+            {
+                if world.ball.position.0 + world.ball.radius > w {
+                    world.p1.score += 1;
+                }
+                if world.ball.position.0 - world.ball.radius < 0.0 {
+                    world.p2.score += 1;
+                }
+            }
+
+            if world.ball.position.1 - world.ball.radius < 0.0 {
+                world.ball.velocity.1 = -world.ball.velocity.1;
+                world.ball.position.1 = world.ball.radius;
+            }
+            if world.ball.position.1 + world.ball.radius > h {
+                world.ball.velocity.1 = -world.ball.velocity.1;
+                world.ball.position.1 = h - world.ball.radius;
+            }
+            if world.ball.position.0 - world.ball.radius < 0.0 {
+                world.ball.velocity.0 = -world.ball.velocity.0;
+                world.ball.position.0 = world.ball.radius;
+            }
+            if world.ball.position.0 + world.ball.radius > w {
+                world.ball.velocity.0 = -world.ball.velocity.0;
+                world.ball.position.0 = w - world.ball.radius;
+            }
+
+            for (column, amount) in [2, 3, 4, 5, 5, 4, 3, 2].iter().enumerate() {
+                for y in 1..*amount {
+                    let x_pos = (column as f64 + 1.0) / 9.0 * w;
+                    let y_pos = h * (y as f64) / (*amount as f64)
+                        + [&world.p1, &world.p2][PLAYERS[column]].position
+                            / [3, 3, 4, 5, 5, 4, 3, 3][column] as f64 * h;
+
+                    if world.ball.position.0 - world.ball.radius < x_pos + 5.0
+                        && world.ball.position.0 + world.ball.radius > x_pos - 5.0
+                        && world.ball.position.1 - world.ball.radius < y_pos + 25.0
+                        && world.ball.position.1 + world.ball.radius > y_pos - 25.0
+                    {
+                        let ball_x = (world.ball.position.0 - x_pos).abs()
+                            * (PLAYERS[column] as f64 * -2.0 + 1.0);
+                        let ball_y = world.ball.position.1 - y_pos;
+                        let length = ball_x.hypot(ball_y);
+                        world.ball.velocity.0 = ball_x / length;
+                        world.ball.velocity.1 = ball_y / length;
+                    }
+
                     rectangle(
-                        colors[column as usize],
-                        [
-                            column as f64 / 7.0 * (w * 6.0 / 10.0) + w * 2.0 / 10.0 - 5.0,
-                            h * (y as f64 + 1.0) / (*amount as f64 + 1.0) - 25.0,
-                            10.0,
-                            50.0,
-                        ],
+                        my_palette[PLAYERS[column]],
+                        [x_pos - 5.0, y_pos - 25.0, 10.0, 50.0],
                         c.transform,
                         g,
                     );
                 }
             }
+
+            rectangle(
+                [0.1, 0.1, 0.1, 1.0],
+                [0.0, h / 3.0, 10.0, h / 3.0],
+                c.transform,
+                g,
+            );
+
+            rectangle(
+                [0.1, 0.1, 0.1, 1.0],
+                [w - 10.0, h / 3.0, 10.0, h / 3.0],
+                c.transform,
+                g,
+            );
+
+            ellipse(
+                [0.1, 0.1, 0.1, 1.0],
+                [
+                    world.ball.position.0 - world.ball.radius,
+                    world.ball.position.1 - world.ball.radius,
+                    world.ball.radius * 2.0,
+                    world.ball.radius * 2.0,
+                ],
+                c.transform,
+                g,
+            );
+
+            text(
+                [0.7, 0.7, 0.7, 1.0],
+                50,
+                &(world.p1.score).to_string(),
+                &mut glyphs,
+                c.transform.trans(w / 2.0 - 50.0, 50.0),
+                g,
+            ).unwrap();
+
+            text(
+                [0.7, 0.7, 0.7, 1.0],
+                50,
+                &(world.p2.score).to_string(),
+                &mut glyphs,
+                c.transform.trans(w / 2.0 + 50.0, 50.0),
+                g,
+            ).unwrap();
         });
     }
 }
