4a98975f2e6559af2bb71179685f1dbf4fb18232
modderme123 <modderme123@gmail.com>
2018-01-20 17:10:03 -0800

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-extern crate piston;
 extern crate graphics;
 extern crate opengl_graphics;
+extern crate piston;
 
 use piston::window::WindowSettings;
 use piston::event_loop::*;
@@ -8,7 +8,7 @@ use piston::input::*;
 extern crate piston_window;
 
 use piston_window::*;
-use opengl_graphics::{ GlGraphics, OpenGL };
+use opengl_graphics::{GlGraphics, OpenGL};
 #[derive(Debug)]
 struct Ball {
     radius: f64,
@@ -34,40 +34,37 @@ const PALLETTES: [[[f32; 4]; 2]; 2] = [
 const PLAYERS: [usize; 8] = [0, 0, 1, 0, 1, 0, 1, 1];
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,   // Rotation for the square.
-    world:World,
-    
+    rotation: f64,  // Rotation for the square.
+    world: World,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
-    let my_palette = PALLETTES[0];
-    let colors: Vec<_> = [0, 0, 1, 0, 1, 0, 1, 1]
-        .iter()
-        .map(|x| my_palette[*x as usize])
-        .collect();
+        let my_palette = PALLETTES[0];
+        let colors: Vec<_> = [0, 0, 1, 0, 1, 0, 1, 1]
+            .iter()
+            .map(|x| my_palette[*x as usize])
+            .collect();
         use graphics::*;
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
-        let mut world=&mut self.world;
-        let mut shrunkScale=(args.width as f64)/(world.size.0 as f64);
-        if((args.height as f64)/(world.size.1 as f64)<shrunkScale){
-            shrunkScale=(args.height as f64)/(world.size.1 as f64);
+        let mut world = &mut self.world;
+        let mut shrunkScale = (args.width as f64) / (world.size.0 as f64);
+        if ((args.height as f64) / (world.size.1 as f64) < shrunkScale) {
+            shrunkScale = (args.height as f64) / (world.size.1 as f64);
         }
-        let (cx, cy) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
-       
+        let (cx, cy) = ((args.width / 2) as f64, (args.height / 2) as f64);
+
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear([0.9, 0.9, 0.9, 1.0], gl);
-             let w = shrunkScale*(world.size.0 as f64);
-                    let h = shrunkScale*(world.size.1 as f64);
-                    let transform = c.transform.trans(cx, cy)
-                                       .trans(-w/2.0, -h/2.0);
+            let w = shrunkScale * (world.size.0 as f64);
+            let h = shrunkScale * (world.size.1 as f64);
+            let transform = c.transform.trans(cx, cy).trans(-w / 2.0, -h / 2.0);
 
             /*for (column, amount) in [1, 2, 3, 4, 4, 3, 2, 1].iter().enumerate() {
                 for y in 0..*amount {
@@ -138,60 +135,56 @@ impl App {
                 }
             }
 
-            
-
             // Draw a box rotating around the middle of the screen.
             //rectangle(RED, square, transform, gl);
         });
     }
-//update game here (args.dt is delta time)
+    //update game here (args.dt is delta time)
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        let mut world=&mut self.world;
-        let step:f64=args.dt*60.0;
+        let mut world = &mut self.world;
+        let step: f64 = args.dt * 60.0;
         let w = (world.size.0 as f64);
-                    let h = (world.size.1 as f64);
+        let h = (world.size.1 as f64);
         world.p1.position += world.p1.dir as f64 * step;
-            world.p2.position += world.p2.dir as f64 * step;
+        world.p2.position += world.p2.dir as f64 * step;
 
-            world.p1.position = world.p1.position.min(1.0).max(-1.0);
-            world.p2.position = world.p2.position.min(1.0).max(-1.0);
+        world.p1.position = world.p1.position.min(1.0).max(-1.0);
+        world.p2.position = world.p2.position.min(1.0).max(-1.0);
 
-            world.ball.position.0 += world.ball.velocity.0 * 5.0 * step;
-            world.ball.position.1 += world.ball.velocity.1 * 5.0 * step;
+        world.ball.position.0 += world.ball.velocity.0 * 5.0 * step;
+        world.ball.position.1 += world.ball.velocity.1 * 5.0 * step;
 
-            if world.ball.position.1 - world.ball.radius > h / 3.0
-                && world.ball.position.1 + world.ball.radius < h * 2.0 / 3.0
-            {
-                if world.ball.position.0 + world.ball.radius > w {
-                    world.p1.score += 1;
-                }
-                if world.ball.position.0 - world.ball.radius < 0.0 {
-                    world.p2.score += 1;
-                }
-            }
-
-            if world.ball.position.1 - world.ball.radius < 0.0 {
-                world.ball.velocity.1 = -world.ball.velocity.1;
-                world.ball.position.1 = world.ball.radius;
-            }
-            if world.ball.position.1 + world.ball.radius > h {
-                world.ball.velocity.1 = -world.ball.velocity.1;
-                world.ball.position.1 = h - world.ball.radius;
+        if world.ball.position.1 - world.ball.radius > h / 3.0
+            && world.ball.position.1 + world.ball.radius < h * 2.0 / 3.0
+        {
+            if world.ball.position.0 + world.ball.radius > w {
+                world.p1.score += 1;
             }
             if world.ball.position.0 - world.ball.radius < 0.0 {
-                world.ball.velocity.0 = -world.ball.velocity.0;
-                world.ball.position.0 = world.ball.radius;
+                world.p2.score += 1;
             }
-            if world.ball.position.0 + world.ball.radius > w {
-                world.ball.velocity.0 = -world.ball.velocity.0;
-                world.ball.position.0 = w - world.ball.radius;
-            }
+        }
+
+        if world.ball.position.1 - world.ball.radius < 0.0 {
+            world.ball.velocity.1 = -world.ball.velocity.1;
+            world.ball.position.1 = world.ball.radius;
+        }
+        if world.ball.position.1 + world.ball.radius > h {
+            world.ball.velocity.1 = -world.ball.velocity.1;
+            world.ball.position.1 = h - world.ball.radius;
+        }
+        if world.ball.position.0 - world.ball.radius < 0.0 {
+            world.ball.velocity.0 = -world.ball.velocity.0;
+            world.ball.position.0 = world.ball.radius;
+        }
+        if world.ball.position.0 + world.ball.radius > w {
+            world.ball.velocity.0 = -world.ball.velocity.0;
+            world.ball.position.0 = w - world.ball.radius;
+        }
     }
     fn key(&mut self, key: Key) {
-        if key == Key::W {
-
-        }
+        if key == Key::W {}
         // Rotate 2 radians per second.
     }
 }
@@ -199,7 +192,7 @@ impl App {
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
-let mut world: World = World {
+    let mut world: World = World {
         size: (1050, 600),
         ball: Ball {
             radius: 0.05,
@@ -218,9 +211,7 @@ let mut world: World = World {
         },
     };
     // Create an Glutin window.
-    let mut window: PistonWindow = WindowSettings::new("Foosball",
-            [world.size.0, world.size.1]
-        )
+    let mut window: PistonWindow = WindowSettings::new("Foosball", [world.size.0, world.size.1])
         .opengl(opengl)
         .exit_on_esc(true)
         .build()
@@ -230,7 +221,7 @@ let mut world: World = World {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
-        world:world
+        world: world,
     };
 
     let mut events = Events::new(EventSettings::new());
